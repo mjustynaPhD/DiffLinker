398bc96a17463ab8d3288c18fd7045216b344300
Marek Justyna <mjustyna@cs.put.poznan.pl>
2023-07-06 06:27:10 +0000

    Data processing update

--- data/rna/data_processing.py
+++ data/rna/data_processing.py
@@ -4,17 +4,21 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
+
+
 from Bio.PDB import *
+from Bio.SVDSuperimposer import SVDSuperimposer
 from rnapolis.annotator import extract_secondary_structure
-from rnapolis.parser import read_3d_structure\
+from rnapolis.parser import read_3d_structure
 
 pdbs_dir = "/data/3d/input_data/pdbs/"
+out_name = 'rna_GC_train.pt'
 ONE_HOT = {'P': 3, 'H': 4, 'C': 0, 'O': 2, 'N': 1}
 
 def main():
     # read 3d structure
     all_transformed = []
-    for pdb in tqdm(os.listdir(pdbs_dir)[120:220]):
+    for pdb in tqdm(os.listdir(pdbs_dir)[:10]):
         pdb_path = os.path.join(pdbs_dir, pdb)
         # print(f"Structure extraction from pdb file {pdb}")
         with open(pdb_path) as f:
@@ -25,7 +29,7 @@ def main():
         interest_bps = []
         for bp in structure2d.basePairs:
             bp_type = bp.saenger.is_canonical if bp.saenger is not None else False
-            if bp_type and bp.nt1.name == 'G' or bp.nt2.name == 'G':
+            if bp_type and bp.nt1.name == 'C' and bp.nt2.name == 'G':
                 # print(f"{bp.nt1.chain}.{bp.nt1.name}{bp.nt1.number}")
                 # print(f"{bp.nt2.chain}.{bp.nt2.name}{bp.nt2.number}")
                 bp1 = f"{bp.nt1.chain}.{bp.nt1.name}{bp.nt1.number}"
@@ -67,11 +71,18 @@ def main():
                 print(f"KeyError: {pdb} {bps[0]} {bps[1]}")
                 raise
             
+            # if len(all_transformed) >2:
+            #     try:
+            #         superimposed = superimpose(all_transformed[0]['positions'], all_transformed[-1]['positions'])
+            #         all_transformed[-1]['positions'] = superimposed
+            #     except Exception as e:
+            #         print("Error in superimpose")
+            #         all_transformed.pop()
 
     # save transformed to *.pt file
-    name = 'rna_GC_val100.pt'
-    torch.save(all_transformed, name)
-    print(f"Saved {name}")
+    
+    torch.save(all_transformed, out_name)
+    print(f"Saved {out_name}")
 
 # %%
 def save_as_new_pdb(residues, output_file):
@@ -107,11 +118,25 @@ def pdb_transform(residues, name):
     # atoms = get_atoms(residues)
     return transform_dict
 
+def superimpose(ref_coords, model_coords):
+    sup = SVDSuperimposer()
+    sup.set(np.array(ref_coords, 'f'), np.array(model_coords, 'f'))
+    sup.run()
+    print('RMSD: {:.2f}'.format(sup.get_rms()))
+    return torch.Tensor(sup.get_transformed())
+
 def get_positions(residues):
     positions = []
     for residue in residues:
         for atom in residue.get_atoms():
             positions.append(atom.get_coord())
+
+    # # normalize the positions to 0 mean and 1 std
+    positions = np.array(positions)
+    positions = (positions - positions.mean(axis=0))
+    # normalize positions to 0-1
+    # positions = np.array(positions)
+    # positions = (positions - positions.min(axis=0)) / (positions.max(axis=0) - positions.min(axis=0))
     return np.array(positions)
 
 def get_one_hot(residues):
@@ -155,7 +180,7 @@ def get_anchors(residues):
     anchors = []
     for residue in residues:
         atom_names = [atom.name for atom in residue.get_atoms()]
-        anchors.extend([1 if atom_name == 'C5\'' else 0 for atom_name in atom_names])
+        anchors.extend([1 if atom_name == 'C1\'' else 0 for atom_name in atom_names])
     return np.array(anchors)
 
 if __name__ == "__main__":
